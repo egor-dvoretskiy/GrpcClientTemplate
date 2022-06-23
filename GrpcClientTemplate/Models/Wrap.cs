5e2d3772e28f042df64044a4a06bc367a2f4033e
ergo.dvoretskiy <ergo.dvoretskiy@gmail.com>
2022-06-23 16:08:47 +0300

    Modified wrap with IssueExplanation for effective debugging.

--- GrpcClientTemplate/Models/Wrap.cs
+++ GrpcClientTemplate/Models/Wrap.cs
@@ -9,24 +9,31 @@ namespace GrpcClientTemplate.Models
     public class Wrap<T>
     {
         private readonly bool _isValid = false;
+        private readonly string _issueExplanation = string.Empty;
 
         public Wrap(T message)
         {
             this.Message = message;
             this._isValid = true;
+
+            this._issueExplanation = "No need in explanation.";
         }
 
-        public Wrap()
+        public Wrap(string issueExplanation = "No explanation")
         {
             this._isValid = false;
+
+            this._issueExplanation = issueExplanation;
+        }
+
+        public string IssueExplanation
+        {
+            get => this._issueExplanation;
         }
 
         public bool IsValid
         {
-            get
-            {
-                return this._isValid;
-            }
+            get => this._isValid;
         }
 
         public T Message { get; private set; }
